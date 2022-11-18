12ec9dbbbdeb059adbe89d310b122b0961e35938
magifdev <marcgibert91@gmail.com>
2022-11-18 15:09:28 +0100

    youtube api search

--- src/components/apis/youtube.js
+++ src/components/apis/youtube.js
@@ -1,1 +1,13 @@
-const KEY = 'AIzaSyCs3NYXy1g - DPPJIBbEWn3E - 2q8vtF0_V4';
+import axios from "axios";
+
+const KEY = 'AIzaSyCs3NYXy1g-DPPJIBbEWn3E-2q8vtF0_V4';
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: KEY,
+  },
+});
